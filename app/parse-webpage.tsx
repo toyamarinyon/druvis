c994cba821f06a5ebcdad5310f2bcec17a98afaa
satoshi toyama <satoshi.toyama@route06.co.jp>
2024-04-10 09:50:58 +0900

    update

--- app/parse-webpage.tsx
+++ app/parse-webpage.tsx
@@ -1,44 +1,48 @@
 export const parseContent = async (url: string) => {
-	const res = await fetch(`${url}`);
+  const res = await fetch(`${url}`);
 
-	let content = "";
-	await new HTMLRewriter()
-		.on("p, h1, h2, h3, h4, h5, h6", {
-			text(text) {
-				content = `${content} ${text.text}`;
-			},
-		})
-		.transform(res)
-		.text();
-	return content.replaceAll(/\n/g, "").replaceAll(/(\s+|&nbsp;)/g, " ");
+  let content = "";
+  await new HTMLRewriter()
+    .on("p, h1, h2, h3, h4, h5, h6", {
+      text(text) {
+        content = `${content} ${text.text}`;
+      },
+    })
+    .transform(res)
+    .text();
+  return content.replaceAll(/\n/g, "").replaceAll(/(\s+|&nbsp;)/g, " ");
 };
 
 export const parseMeta = async (fetchUrl: string) => {
-	const url = new URL(fetchUrl);
-	const res = await fetch(url);
+  const url = new URL(fetchUrl);
+  const res = await fetch(url);
 
-	let title: string | null;
-	let description: string | null;
-	const host = url.host;
-	await new HTMLRewriter()
-		.on("title", {
-			text: (el) => {
-				title = el.text;
-			},
-		})
-		.on("meta", {
-			element(el) {
-				if (el.getAttribute("name") === "description") {
-					description = el.getAttribute("content");
-				}
-			},
-		})
-		.transform(res)
-		.text();
-	return {
-		title,
-		description,
-		host,
-		favicon: "/favicon.ico",
-	};
+  let title: string | null;
+  let description: string | null;
+  const host = url.host;
+  await new HTMLRewriter()
+    .on("head title", {
+      text: ({ text, lastInTextNode }) => {
+        if (lastInTextNode) {
+          return;
+        }
+        title = text;
+      },
+    })
+    .on("meta", {
+      element(el) {
+        if (el.getAttribute("name") === "description") {
+          description = el.getAttribute("content");
+        }
+      },
+    })
+    .transform(res)
+    .text();
+  const favicon = `${url.origin}/favicon.ico`;
+  return {
+    title,
+    description,
+    host,
+    favicon,
+  };
 };
--- app/routes/s.$id.tsx
+++ app/routes/s.$id.tsx
@@ -1,69 +1,111 @@
-import { type LoaderFunctionArgs, json } from "@remix-run/cloudflare";
+import {
+  type LoaderFunctionArgs,
+  json,
+  MetaFunction,
+} from "@remix-run/cloudflare";
 import { Link, useLoaderData } from "@remix-run/react";
 import { CornerDownLeftIcon } from "lucide-react";
 import Markdown from "react-markdown";
 import { drizzle } from "~/db/drizzle";
 
 export const loader = async (args: LoaderFunctionArgs) => {
-	const id = args.params.id;
+  const id = args.params.id;
 
-	if (id == null) {
-		throw new Response(null, {
-			status: 404,
-			statusText: "Not Found",
-		});
-	}
-	const db = drizzle(args);
-	const webpage = await db.query.webpages.findFirst({
-		where: (webpages, { eq }) => eq(webpages.id, id),
-		with: {
-			summary: true,
-			keywords: true,
-		},
-	});
-	if (webpage == null) {
-		throw new Response(null, {
-			status: 404,
-			statusText: "Not Found",
-		});
-	}
-	return json({ webpage });
+  if (id == null) {
+    throw new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  const db = drizzle(args);
+  const webpage = await db.query.webpages.findFirst({
+    where: (webpages, { eq }) => eq(webpages.id, id),
+    with: {
+      header: true,
+      summary: true,
+      keywords: true,
+    },
+  });
+  if (webpage == null) {
+    throw new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  return json({ webpage });
+};
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (data?.webpage == null) {
+    return [];
+  }
+  return [
+    {
+      title: `要約: ${data.webpage.header?.title} | ${data.webpage.header?.host}`,
+    },
+    {
+      property: "og:title",
+      content: `要約: ${data.webpage.header?.title}`,
+    },
+    {
+      name: "description",
+      content: data.webpage.summary?.summaryText,
+    },
+  ];
 };
 
 export default function SummaryPage() {
-	const { webpage } = useLoaderData<typeof loader>();
-	return (
-		<div className="text-gray-300 border-l pl-4 py-2 border-gray-500 w-[800px] max-h-[77%] flex flex-col">
-			<div className="flex gap-2 flex-1 h-auto overflow-hidden">
-				<div className="w-[67%]">
-					<p className="text-blue-500">要約結果:</p>
-					<Markdown className="overflow-y-scroll h-full pb-8">
-						{webpage.summary?.summaryText}
-					</Markdown>
-				</div>
-				<div className="flex-1">
-					<p className="text-blue-500">重要キーワード（英語）:</p>
-					<ul className="text-sm list-inside list-disc overflow-y-scroll h-full gap-2 pb-8">
-						{webpage.keywords.map(({ id, content, description }) => (
-							<li key={id}>
-								<span className="text-gray-200">{content}</span>
-								<p className="ml-5 text-gray-500 ">{description}</p>
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-			<div className="flex items-center gap-2 shrink-0">
-				<Link to="/">
-					<button
-						className="text-gray-500 hover:text-gray-400 transition-all flex items-center gap-1"
-						type="button"
-					>
-						<CornerDownLeftIcon strokeWidth={1} className="w-4 h-4" />
-						戻る
-					</button>
-				</Link>
-			</div>
-		</div>
-	);
+  const { webpage } = useLoaderData<typeof loader>();
+  return (
+    <div className="text-gray-300 border-l pl-4 py-2 border-gray-500 w-[800px] max-h-[77%] flex flex-col">
+      <div className="border-b mb-2">
+        <div className="p-2">
+          <div className="flex gap-1 items-center">
+            {webpage.header?.favicon != null && (
+              <img
+                src={webpage.header.favicon}
+                alt="favicon"
+                className="w-4 h-4"
+              />
+            )}
+            {webpage.header?.host && (
+              <p className="text-sm">{webpage.header.host}</p>
+            )}
+          </div>
+          <h1 className="text-xl font-bold">{webpage.header?.title}</h1>
+          {webpage.header?.description && <p>{webpage.header.description}</p>}
+        </div>
+      </div>
+      <div className="flex gap-2 flex-1 h-auto overflow-hidden">
+        <div className="w-[67%]">
+          <p className="text-blue-500">要約:</p>
+          <Markdown className="overflow-y-scroll h-full pb-8">
+            {webpage.summary?.summaryText}
+          </Markdown>
+        </div>
+        <div className="flex-1">
+          <p className="text-blue-500">重要キーワード（英語）:</p>
+          <ul className="text-sm list-inside list-disc overflow-y-scroll h-full gap-2 pb-8">
+            {webpage.keywords.map(({ id, content, description }) => (
+              <li key={id}>
+                <span className="text-gray-200">{content}</span>
+                <p className="ml-5 text-gray-500 ">{description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 shrink-0 pt-2">
+        <Link to="/">
+          <button
+            className="text-gray-500 hover:text-gray-400 transition-all flex items-center gap-1"
+            type="button"
+          >
+            <CornerDownLeftIcon strokeWidth={1} className="w-4 h-4" />
+            戻る
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
